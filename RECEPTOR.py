b9c41986b7f6d55483a6b2f724bcf717b6515b27
Jose Alfredo Cortes Quiroz <40708943+leoalfredcortes@users.noreply.github.com>
2019-11-22 16:13:38 -0600

    Update RECEPTOR.py

--- RECEPTOR.py
+++ RECEPTOR.py
@@ -23,25 +23,36 @@ port = 6000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 membership = socket.inet_aton(multicast_addr) + socket.inet_aton(bind_addr)
-
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
 sock.bind((bind_addr, port))
 
 window = Tk() 
-window.title("..::Aletas de Temperatura UDP::..") 
-window.geometry('430x430')  
+window.title("..::Receptor Aletas de Temperatura UDP::..") 
+window.geometry('425x450')  
 area_conversation = scrolledtext.ScrolledText(window,width=50,height=20)
 area_conversation.grid(column=1, row=0)
+falg_start = 0
 
 def get_alerts():
-    while True:
+    falg_start = 1
+    while(falg_start==1):
         message, address = sock.recvfrom(255)
         area_conversation.insert(INSERT,message)
         area_conversation.insert(INSERT,'\n')
-        print (message)
+        
+def clean_text():
+    area_conversation.delete('1.0', END)    
+    
+def ini_prog():
+    area_conversation.insert(INSERT,"Iniciando sistema de Alertas...")
+    tread1 = threading.Thread(target = get_alerts)
+    tread1.start()    
+    
+btn = Button(window, text="Start", bg="blue", fg="white", command=ini_prog)
+btn.grid(column=1, row=2)
 
-tread1 = threading.Thread(target = get_alerts)
-tread1.start()
+btn = Button(window, text="Clear", bg="blue", fg="white", command=clean_text)
+btn.grid(column=1, row=3)
+
 window.mainloop()
