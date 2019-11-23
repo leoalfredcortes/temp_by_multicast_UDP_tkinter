f0bf21ceeae6b08bf4fee634f54ebf097e7dd06f
Jose Alfredo Cortes Quiroz <40708943+leoalfredcortes@users.noreply.github.com>
2019-11-23 01:47:55 -0600

    Update RECEPTOR.py

--- RECEPTOR.py
+++ RECEPTOR.py
@@ -2,8 +2,8 @@
 # Instituto Tecnológico de Colima - División de Estudios de Posgrado e Investigación #
 # Maestría en Sistemas Computacionales - Materia: Tecnologías de Internet            #
 #                                                                                    #
-# Código Fuente para Cliente (receptor) UDP, con interfas tkinter el cual obtiene    #
-# alertas si la temperatura exede de los rangos establesidos en el servidor (emisor) #
+# Código Fuente para Cliente (receptor) UDP, con interfáz tkinter el cual obtiene    #
+# alertas si la temperatura pasa los rangos establesidos en el servidor (emisor)     #
 #                                                                                    #
 # Realizado por:                                                                     #
 # Osvaldo Vladimir Rodríguez Leal                                                    #
@@ -11,48 +11,50 @@
 # Villa de Álvarez, Col 21/10/19                                                     #
 ######################################################################################
 
-from tkinter import *
-from tkinter import scrolledtext
-import threading
-import socket
-import struct
+from tkinter import *                         # Importamos libreria gráfica Tkinter
+from tkinter import scrolledtext              # Importamos el elemento scrolledtext de la libreria Tkinter
+import threading                              # Importamos libreria para manejar Hilos
+import socket                                 # Importamos Libreria para manejar comunicación por Sockets 
+import struct                                 # Importamos Librearia para convertir cadenas de bytes
 
-multicast_addr = '224.0.0.1'
-bind_addr = '0.0.0.0'
-port = 6000
+multicast_addr = '224.0.0.1'                  # Definimos la dirección (grupo) Multicast al que pertenece la interfaz
+bind_addr = '0.0.0.0'                         # Definimos la IP Host al cual se conectara  
+port = 6000                                   # Definimos el puerto por el cual se establesera la comunicación
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-membership = socket.inet_aton(multicast_addr) + socket.inet_aton(bind_addr)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind((bind_addr, port))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)                     # Configuramos el socket para establecer comunicación por protocolo UDP
+membership = socket.inet_aton(multicast_addr) + socket.inet_aton(bind_addr) # Configuramos el grupo al que pertenecerá el receptor 
 
-window = Tk() 
-window.title("..::Receptor Aletas de Temperatura UDP::..") 
-window.geometry('425x450')  
-area_conversation = scrolledtext.ScrolledText(window,width=50,height=20)
-area_conversation.grid(column=1, row=0)
-falg_start = 0
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)    # Cargamos la configuración del grupo multicast al socket
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)                  # terminamos con la configuración del socket para el enlace
 
-def get_alerts():
-    falg_start = 1
-    while(falg_start==1):
-        message, address = sock.recvfrom(255)
-        area_conversation.insert(INSERT,message)
-        area_conversation.insert(INSERT,'\n')
+sock.bind((bind_addr, port))                  # Realizamos en enlace                                          
+
+window = Tk()                                 # Inicializamos interfaz gráfica Tkinter 
+window.title("..::Receptor Aletas de Temperatura UDP::..")                  # Asignamos un titulo a la ventana creada            
+window.geometry('425x450')                                                  # Definimos el tamaño de la ventana
+area_conversation = scrolledtext.ScrolledText(window,width=50,height=20)    # creamos un control tipo scrolledtext y determinamos el largo y ancho
+area_conversation.grid(column=1, row=0)                                     # situamos el scrolledtext en la ventana
+falg_start = 0                                                              # variable para utilizar como bandera
+
+def get_alerts():                                                           # función para recibir las alertas
+    falg_start = 1                                                          # cambiamos el estatus de nuestra bandera a 1    
+    while(falg_start==1):                                                   # Iniciamos un siclo hasta que nuestra bandera cambie
+        message, address = sock.recvfrom(255)                               # cargamos la alerta recibida en la variable message
+        area_conversation.insert(INSERT,message)                            # Insertamos en scrolledtext la alerta recibida 
+        area_conversation.insert(INSERT,'\n')                               # Insertamos en scrolledtext un salto de linea
         
-def clean_text():
-    area_conversation.delete('1.0', END)    
+def clean_text():                                                           # Función para limpiar scrolledtext
+    area_conversation.delete('1.0', END)                                    # Borramos contenido de scrolledtext    
     
-def ini_prog():
-    area_conversation.insert(INSERT,"Iniciando sistema de Alertas...")
-    tread1 = threading.Thread(target = get_alerts)
-    tread1.start()    
+def ini_prog():                                                             # Función que inicia la recepción de alertas
+    area_conversation.insert(INSERT,"Iniciando sistema de Alertas...")      # Introducimos mensaje de inicio en scrolledtext
+    tread1 = threading.Thread(target = get_alerts)                          # Iniciamos la función de recibir alertas a hilos
+    tread1.start()                                                          # Iniciamos hilos    
     
-btn = Button(window, text="Start", bg="blue", fg="white", command=ini_prog)
-btn.grid(column=1, row=2)
+btn = Button(window, text="Start", bg="blue", fg="white", command=ini_prog) # Creamos Botón, asignamos texto, ponemos azul el color de fondo y lo asociamos con la funcion para inicio de alertas
+btn.grid(column=1, row=2)                                                   # Situamos el Botón en la ventana    
 
-btn = Button(window, text="Clear", bg="blue", fg="white", command=clean_text)
-btn.grid(column=1, row=3)
+btn = Button(window, text="Clear", bg="blue", fg="white", command=clean_text) # Creamos Botón, asignamos texto, ponemos azul el color de fondo y lo asociamos con la funcion para limpiar scrolledtext
+btn.grid(column=1, row=3)                                                   # Situamos el Botón en la ventana
 
-window.mainloop()
+window.mainloop()                               # Ponemos en un siclo infinito la Interfaz creada en espera de algún evento.
